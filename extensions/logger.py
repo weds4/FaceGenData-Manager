04166c2154b77021bba3b052b36c125bf4808159
weds4 <36106902+weds4@users.noreply.github.com>
2021-05-18 15:45:11 -0500

    less namespace usage?

--- extensions/logger.py
+++ extensions/logger.py
@@ -3,22 +3,22 @@ from time import localtime
 from time import strftime
 CRED = '\033[93m'
 CEND = '\033[0m'
-errorLog = {
-    "noND":
-        """The message \"nif/dds not found\" means that the program did not find\
+def errorLog(etype):
+    if etype == "noND":
+        return """The message \"nif/dds not found\" means that the program did not find\
  any nif or dds files to hide.\nThere are several reasons this can happen:
   1. The files are already hidden. You may have already covered this NPC \
 previously.
   2. The no other loaded mods have nifs/dds's for that NPC.
   3. The NPC is missing the nif/dds from the mods you are trying to overwrite""",
 
-    "NoMO2":
-        "You did not choose a valid folder for your current MO2 profile"
-    }
+    if etype == "NoMO2":
+        return "You did not choose a valid folder for your current MO2 profile"
+
 
 def logDebugInfo(errorCode):
     with open("NPC_Manager.log", "a+") as logfile:
-        logfile.write(errorLog[errorCode]+'\n')
+        logfile.write(errorLog(errorCode)+'\n')
 
 def addLineStart(line):
     ts = localtime()
